52135bb1c1a958111897b38726cac23de80ae555
arly-p0 <ttaarlette@gmail.com>
2023-03-18 02:41:29 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,7 +13,7 @@ int main(void)
 	for (d = '0'; d <= '9'; d++)
 	putchar(d);
 
-	for (low = 'a'; low <= 'f'; l0w++)
+	for (low = 'a'; low <= 'f'; low++)
 	putchar(low);
 	putchar('\n');
 	return (0);
