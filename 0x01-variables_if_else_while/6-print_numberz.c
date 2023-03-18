b43ae83c6c2d6b64a2bfd3576c5c0873ed7f5284
arly-p0 <ttaarlette@gmail.com>
2023-03-18 01:49:18 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+	}
+	putchar('\n');
+	return (0);
