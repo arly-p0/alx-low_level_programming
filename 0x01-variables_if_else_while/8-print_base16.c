6afd2b2eea17d2c48668f7aa079a6b1bd6401a40
arly-p0 <ttaarlette@gmail.com>
2023-03-18 02:16:12 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entery point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int d;
+	char low;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(low);
+	putchar('\n);
+	return (0);
+}
