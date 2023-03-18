e2fa1077b73738a1715c86f8437a39923719f0b2
arly-p0 <ttaarlette@gmail.com>
2023-03-18 01:02:22 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets lower and uppercase
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i <= 122)
+	{
+		putchar(n);
+	}
+	while (j <= 90)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
