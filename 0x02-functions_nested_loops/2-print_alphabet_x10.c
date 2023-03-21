bb32a7862dba8fa6a0fb381e061ed91341ef4ca2
arly-p0 <ttaarlette@gmail.com>
2023-03-21 22:48:44 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10x alphabets
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+	i++;
+	}
