69e89512eea52b5cfa4f46e439662f0033418351
arly-p0 <ttaarlette@gmail.com>
2023-03-22 17:03:56 +0000

    Nature made the natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Prints the sum of all multiples of 3 or 5 up to 1824
+ * main - Prints the sum of all multiples of 3 or 5 up to 1024
  * Return: Always 0
  */
 int main(void)
 {
 	int i, z = 0;
 
-	while (i < 1824)
+	while (i < 1024)
 	{
 	if ((i % 3 == 0) || (i % 5 == 0))
-	{		
+	{
 	z += i;
 	}
 	i++;
