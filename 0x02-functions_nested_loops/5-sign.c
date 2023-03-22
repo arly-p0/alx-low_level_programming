47e71d6103cf804bce9ca276f7c836961945236d
arly-p0 <ttaarlette@gmail.com>
2023-03-22 00:01:29 +0000

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs
+ * @n: function parameter
+ * Return: 1 for positive number, -1 for negative number or zero for otherwise
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+	{
+	_putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	}
+	else
+	{
+	_putchar(48);
+	return (0);
+	}
+}
