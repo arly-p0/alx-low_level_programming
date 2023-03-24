6cc2d3f8b1907498bc8616cd5ca375b26a7f6d72
arly-p0 <ttaarlette@gmail.com>
2023-03-24 09:22:04 +0000

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,17 +8,30 @@
 
 int main(void)
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+	long int n;
+	long int max;
+	long int i;
 
-	for (x = 1; x <= square; x++)
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-	if (number % x == 0)
+		max = 2;
+		n /= 2;
+	}
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-	maxf = number / x;
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	}
-	printf("%ld\n", maxf);
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
 	return (0);
 }
