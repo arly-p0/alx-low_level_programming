4becf09a12b4f0fd5d27aa386043135630938f52
arly-p0 <ttaarlette@gmail.com>
2023-03-30 13:33:28 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,6 +16,6 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; src[i] != '\0'; i++)
 		srclen++;
 	for (i = 0; i <= srclen; i++)
-		dest[destlen + 1] = src[i];
+		dest[destlen + i] = src[i];
 	return (dest);
 }
