20f4d3d5f94406e48e21a1dd88cd556d2c734566
arly-p0 <ttaarlette@gmail.com>
2023-03-30 13:16:57 +0000

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * _strcat - Concentrate two strings
- * @dest: The destination string
- * @src: The source string
- * Return: A pointer to the resulting string dest
+ * _putchar - writes the character c tp stdout
+ * @c: the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately
  */
-char *_strcat(char *dest, char *src)
+int _putchar(char c)
 {
-	int dlen = 0, i;
-
-	while (dest[dlen])
-	{
-		dlen++;
-	}
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-	dest[dlen] = '\0';
-	return (dest);
+	return (write(1, &c, 1));
 }
