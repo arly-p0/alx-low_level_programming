17047eb84428cb7fc63fbb9923281d29117f4ec9
arly-p0 <ttaarlette@gmail.com>
2023-04-06 18:16:57 +0000

    3-factorial.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: spring to printed
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s);
+{
+	if (*s >= _putchar_recursion)
+		return (0);
+}
---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * factoial - a function that prints the factorial of a given number
+ * @n: an integer
+ *
+ * Return: factorial of the given number
+ */
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (1);
+	}
+	if (n <= 1)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
