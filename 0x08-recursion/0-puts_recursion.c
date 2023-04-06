e75f4cc3872ba6e4eedb83a8dc2f0ee70d9271dc
arly-p0 <ttaarlette@gmail.com>
2023-04-06 19:55:31 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,8 +5,15 @@
  *
  * Return: Always 0.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-	if (*s >= _putchar_recursion)
-		return (0);
+	if (*s != '\n' && *s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - function that returns the input of a prime number
+ * @n: the input of the integer
+ *
+ * Return: 1 if input of the integer is a prime number, otherwise 0.
+ */
+int is_prime_number(int n)
+{
+	if (*s != '\n' && *s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
