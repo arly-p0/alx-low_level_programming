3b93364d61b547610273e7cebaea7eb618e47833
arly-p0 <ttaarlette@gmail.com>
2023-04-13 19:26:42 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,35 +11,33 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptrs;
-	unsigned int k = 0, l = 0, m = 0, p = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[k] != '\0')
-		k++;
-	while (s2[l] != '\0')
+	unsigned int l, y, z, k;
+	
+	l = y = z = k = 0;
+	while (s1[l])
 		l++;
-	if (n >= m)
-		p = k + m;
+	while (s2[z])
+		z++;
+	if (n >= z)
+		k = l + z;
 	else
-		p = k + n;
+		k = l + n;
 
-	ptrs = malloc(sizeof(char) * p + 1);
+	ptrs = malloc(sizeof(char) * (l + 1));
 
 	if (ptrs == NULL)
 		return (NULL);
-	m = 0;
-	while (l < p)
+	while (y < k)
 	{
-		if (l <= k)
-			ptrs[l] = s1[l];
-		if (l >= k)
-			ptrs[l] = s2[m];
-		m++;
-		l++;
+		if (y <= k)
+			ptrs[y] = s1[y];
+		if (y >= l)
+		{
+			ptrs[y] = s2[z];
+			z++;
+		}
+		y++;
 	}
-	ptrs[l] = '\0';
+	ptrs[y] = '\0';
 	return (ptrs);
 }
