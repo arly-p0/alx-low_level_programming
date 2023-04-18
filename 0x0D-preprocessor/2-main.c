96cb8cd2549a1c5b20a6b47894a395fec56b7d6d
arly-p0 <ttaarlette@gmail.com>
2023-04-18 00:25:48 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the name of a file
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("file define as: %s\n", __FILE__);
+	return (0);
+}
