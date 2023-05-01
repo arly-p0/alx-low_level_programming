ec4270589a2bae509a4d82dbd9b3c5b9aab0cdbf
arly-p0 <ttaarlette@gmail.com>
2023-05-01 17:37:25 +0000

    this function that frees a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -4,7 +4,7 @@
 
 /**
  * free_listint - function that frees a list
- * @head: a pointer to be used in the list
+ * @head: a pointer to be used in the list.
  *
  * Return: Always 0.
  */
@@ -13,7 +13,7 @@ void free_listint(listint_t *head)
 	listint_t *ptrs;
 
 	ptrs = head;
-	while (head == NULL)
+	while (head != NULL)
 	{
 		head = (*head).next;
 		free(ptrs);
